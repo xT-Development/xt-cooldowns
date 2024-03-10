bb5972645368b2fbf70e5aec7f47cc8b7ae50952
xThrasherrr <thrashergamezz@gmail.com>
2024-03-10 14:04:59 -0600

    fix: identation

--- cooldowns.sql
+++ cooldowns.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `cooldowns` (
-   `id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	`length` INT(11) NULL DEFAULT '10',
-	`active` INT(11) NULL DEFAULT '0',
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
+    `length` INT(11) NULL DEFAULT '10',
+    `active` INT(11) NULL DEFAULT '0',
     PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8_general_ci'
